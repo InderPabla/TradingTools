a04a270748ed1bc039f82b753e1334c3cdb3f1c6
inderpabla <inderpreet.pabla@gmail.com>
2021-05-03 19:36:55 -0400

    Modified react-stockcharts library to add thickness lines

--- apps/practice-tool/src/Page/PracticeTool/PracticeTool.tsx
+++ apps/practice-tool/src/Page/PracticeTool/PracticeTool.tsx
@@ -49,25 +49,25 @@ export class PracticeTool extends React.Component<PracticeToolProps,PracticeTool
                                         ref={(ref) => this.divRef = ref}
                                         className="practice-tool-col">
 
-                                        {/* {this.divRef && <PracticeToolOLD width={this.divRef.clientWidth} height={this.divRef.clientHeight}/>} */}
+                                        {this.divRef && <PracticeToolOLD width={this.divRef.clientWidth} height={this.divRef.clientHeight}/>}
                                     </div>
                                 </Col>
                                 <Col xl={6} className="practice-tool-col-container">
                                     <div className="practice-tool-col">
-                                        {/* {this.divRef && <PracticeToolOLD width={this.divRef.clientWidth} height={this.divRef.clientHeight}/>} */}
+                                        {this.divRef && <PracticeToolOLD width={this.divRef.clientWidth} height={this.divRef.clientHeight}/>}
                                     </div>
                                 </Col>
                             </Row>
                             <Row xl={12} noGutters={true} className="flex-nowrap practice-tool-row">
                                 <Col xl={6} className="practice-tool-col-container">
                                     <div className="practice-tool-col">
-                                        {/* {this.divRef && <PracticeToolOLD width={this.divRef.clientWidth} height={this.divRef.clientHeight}/>} */}
+                                        {this.divRef && <PracticeToolOLD width={this.divRef.clientWidth} height={this.divRef.clientHeight}/>}
                                     </div>
                                 </Col>
                                 <Col xl={6} className="practice-tool-col-container">
                                     <div className="practice-tool-col">
                                         {/* <h1 style={{fontSize:"4vh"}}>Chart 4</h1> */}
-                                        {/* {this.divRef && <PracticeToolOLD width={this.divRef.clientWidth} height={this.divRef.clientHeight}/>} */}
+                                        {this.divRef && <PracticeToolOLD width={this.divRef.clientWidth} height={this.divRef.clientHeight}/>}
                                     </div>
                                 </Col>
                             </Row>
