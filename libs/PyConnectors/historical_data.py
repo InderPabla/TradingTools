16ad98f07d2ef924e83110d6e4c45b8ed58a24bb
inderpabla <inderpreet.pabla@gmail.com>
2021-06-28 00:11:39 -0400

    Trading data for MARA on 2021-05-12

--- libs/PyConnectors/historical_data.py
+++ libs/PyConnectors/historical_data.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     #https://interactivebrokers.github.io/tws-api/historical_limitations.html
     
     #Port:7497 for TWS, 4002 fpr IB Gateway
-    ibkr = InteractiveBrokerConnector({'ip_address':'127.0.0.1','port':4002,'client_id':1235})
+    ibkr = InteractiveBrokerConnector({'ip_address':'127.0.0.1','port':4002,'client_id':1234})
     
     header = ['date','open','high','low','close','average','volume','count']
     path = 'D:/Users/InderTheGreat/Documents/Github/TradingTools/services/practice-tool-ms/public/data/'
@@ -31,12 +31,12 @@ if __name__ == '__main__':
     today_month = 6
     today_day = 23
     
-    today_month = 3
-    today_day = 23
+    today_month = 5
+    today_day = 12
     
     
     #'QQQ','SPY','NIO',
-    tickers = ['QQQ','SPY','PLTR']
+    tickers = ['QQQ','SPY','RIOT']
     primary_exchange = {'ALF':'NASDAQ','SPCE':'NYSE'}
     
     end_date = datetime.datetime(today_year, today_month, today_day, 23, 59, 59)
