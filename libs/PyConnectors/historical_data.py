b1fe7a3da79c03de78628958430310730ecc46bc
inderpabla <inderpreet.pabla@gmail.com>
2021-07-20 19:37:06 -0400

    Trading data for 2021-07-20

--- libs/PyConnectors/historical_data.py
+++ libs/PyConnectors/historical_data.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     
     today_year = 2021
     today_month = 7
-    today_day = 19
+    today_day = 20
     
  
     #'QQQ','SPY','NIO','AMC','AAPL','CCL','PLTR','AAL','QFIN','PLUG','TIGR','NVDA
@@ -38,8 +38,9 @@ if __name__ == '__main__':
     #'QQQ','SPY','NIO','SPCE','DIDI','DTSS','ATOS','OXY','PLUG','JZXN','XELA','AAPL'
     #'QQQ','SPY','NIO','SPCE','DIDI','AAPL','CCL','TLRY','BABA','NFLX','CHPT','XPEV'
     #'QQQ','SPY','NIO','SPCE','FUBO','CCIV','FGEN','ERIC','XELA','MRIN','LIZI','AAPL','NVDA','MRNA','X','SGOC','TSLA'
+    #'QQQ','SPY','CCL','AAL', 'OXY', 'AMC', 'SPCE', 'CYTK', 'NIO', 'XELA', 'BB'
     
-    tickers = ['QQQ','SPY','CCL','AAL', 'OXY', 'AMC', 'SPCE', 'CYTK', 'NIO', 'XELA', 'BB']
+    tickers = ['QQQ','SPY','TAL','CCIV','CCL','SGOC','AHPI','MARA','AMC','SPCE','NIO']
     primary_exchange = {'ALF':'NASDAQ','SPCE':'NYSE','ABNB':'NASDAQ','MINM':'NASDAQ'}
     
     end_date = datetime.datetime(today_year, today_month, today_day, 23, 59, 59)
