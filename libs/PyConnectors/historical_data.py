6e582cb2864ac1c591c9e54f7a1b508317361ea3
inderpabla <inderpreet.pabla@gmail.com>
2021-08-30 23:53:19 -0400

    Trading data for 2021-08-30

--- libs/PyConnectors/historical_data.py
+++ libs/PyConnectors/historical_data.py
@@ -69,8 +69,9 @@ if __name__ == '__main__':
     #'QQQ','SPY','AMC','SPCE','KC','DKNG','UBER'
     #'QQQ','SPY','INO','SAVA','AMC','NIO','SLQT','JWN','RIDE'
     #'QQQ','SPY','GPS','DVAX','SAVA','BIG','HPQ','YY','AMC','TKAT','SPRT','CEMI'
+    #'QQQ','SPY','SPRT','WKHS','NURO','WISH','AMC','ATER'
     
-    tickers = ['QQQ','SPY','GPS','DVAX','SAVA','BIG','HPQ','YY','AMC','TKAT','SPRT','CEMI']
+    tickers = ['QQQ','SPY','SPRT','WKHS','NURO','WISH','AMC','ATER']
     primary_exchange = {'ALF':'NASDAQ','SPCE':'NYSE','ABNB':'NASDAQ','MINM':'NASDAQ','OPEN':'NASDAQ','CSCO':'NASDAQ'}
     
     end_date = datetime.datetime(today_year, today_month, today_day, 23, 59, 59)
