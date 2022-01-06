1ce493dab1736b12562815bb48704e77c7d85b4c
inderpabla <inderpreet.pabla@gmail.com>
2022-01-05 19:42:21 -0500

    Trading data up to 2022-01-05

--- libs/PyConnectors/historical_data.py
+++ libs/PyConnectors/historical_data.py
@@ -31,9 +31,9 @@ if __name__ == '__main__':
     today_year = today.year
     today_month = today.month
     today_day = today.day
-    today_year = 2021
-    today_month = 12
-    today_day = 31
+    #today_year = 2021
+    #today_month = 12
+    #today_day = 31
     
     print("Year:"+str(today_year)+", Month:"+str(today_month)+", Day:"+str(today_day))
  
@@ -123,7 +123,10 @@ if __name__ == '__main__':
     #'QQQ','SPY','FCEL','MARA','RIOT','NIO','CCL','ISIG','PLUG'
     #'QQQ','SPY','NIO','VALE','ISIG','LCID','X'
     #'QQQ','SPY','NIO','CCL','SOFI','LCID','ISIG'
-    tickers = ['QQQ','SPY','NIO','CCL','SOFI','LCID','ISIG']
+    #'QQQ','SPY','NIO','LCID','XPEV','F','X','CCL'
+    #'QQQ','SPY','NIO','CCL','LCID','AMD'
+    #'QQQ','SPY','NIO','F','X','AMD','LCID','PFE'
+    tickers = ['QQQ','SPY','NIO','F','X','AMD','LCID','PFE']
     primary_exchange = {'ALF':'NASDAQ','SPCE':'NYSE','ABNB':'NASDAQ','MINM':'NASDAQ','OPEN':'NASDAQ','CSCO':'NASDAQ','LIFE':'NASDAQ','RFL':'NYSE','TIGR':'NASDAQ','PLAN':'NYSE'}
     
     end_date = datetime.datetime(today_year, today_month, today_day, 23, 59, 59)
