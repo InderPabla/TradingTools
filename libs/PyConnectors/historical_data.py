1658ec2a965b3fcc31831b409f01f790e4f0958a
inderpabla <inderpreet.pabla@gmail.com>
2022-06-07 19:19:05 -0400

    Trading data up-to 2022-06-07

--- libs/PyConnectors/historical_data.py
+++ libs/PyConnectors/historical_data.py
@@ -153,7 +153,11 @@ if __name__ == '__main__':
     #'QQQ','SPY','NIO','AAL','TWTR','TSLA'
     #'QQQ','SPY','NIO','AAL','AMD'
     #'QQQ','SPY','NIO','OXY','AMD'
-    tickers = ['OXY','NIO','AMC']
+    #'QQQ','SPY','NIO','OXY','AMC'
+    #'QQQ','SPY','NIO','OXY','TWTR','KSS'
+    #'QQQ','SPY','NIO','OXY','TWTR','XPEV','FUTU','SAVE'
+    #'QQQ','SPY','NIO','KSS','AFRM','AMZN','NVDA'
+    tickers = ['QQQ','SPY','NIO','KSS','AFRM','AMZN','NVDA']
     primary_exchange = {'ALF':'NASDAQ','SPCE':'NYSE','ABNB':'NASDAQ','MINM':'NASDAQ','OPEN':'NASDAQ','CSCO':'NASDAQ','LIFE':'NASDAQ','RFL':'NYSE','TIGR':'NASDAQ','PLAN':'NYSE'}
     
     end_date = datetime.datetime(today_year, today_month, today_day, 23, 59, 59)
